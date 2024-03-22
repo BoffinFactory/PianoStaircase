ce3011b157621515ac3fbef4885f7fbade840647
Braden Pennie <pennie.3@wright.edu>
2024-03-22 15:58:35 -0400

    Deleted useless files

--- src/fluidsynth/start_fluid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to start FluidSynth & aconnect
-echo Attempting to start FluidSynth
-amixer cset numid=3 1
-fluidsynth -si -f /home/mkijowski/git/PianoStaircase/src/fluidsynth/config.txt -a alsa -m alsa_seq /usr/share/sounds/sf2/FluidR3_GM.sf2 &
-sleep 2
-aconnect 20:0 128:0
